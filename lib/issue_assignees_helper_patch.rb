319105c75f3c7216a7c2d36fba3300b063a153da
Michel Beloshitsky <mbeloshitsky@gmail.com>
2014-10-29 17:20:03 +0600

    Глобальная выборка переведена с пользоватлей на секьюрити принципалов (это более правильное решение) closes #968

--- lib/issue_assignees_helper_patch.rb
+++ lib/issue_assignees_helper_patch.rb
@@ -5,11 +5,18 @@ class IssueObserver < ActiveRecord::Observer
 
   	def before_save(issue)
   		project = Project.find(issue.project_id)
-  		if (!project.members.where(:user_id => issue.assigned_to.id).first)
+  		if (issue.assigned_to && !project.members.where(:user_id => issue.assigned_to.id).first)
 			member = Member.new 
 			member.user = issue.assigned_to
 			member.project = project
 			member.roles = [Role.find_by_id(4)] # Nasty selecting developer
+ 			member.save
+		end
+		if (issue.author && !project.members.where(:user_id => issue.author.id).first)
+			member = Member.new 
+			member.user = issue.author
+			member.project = project
+			member.roles = [Role.find_by_id(3)] # Nasty selecting manager
  			member.save
 		end
 	    return true
@@ -27,8 +34,15 @@ module IssueAssigneesHelperPatch
 
 	module InstanceMethods
 		def assignable_users_with_global
-			users = User.all
-			users.uniq.sort
+			users = Principal.where(
+				:status => Principal::STATUS_ACTIVE, 
+				:type => Setting.issue_group_assignment? ? ['User', 'Group'] : ['User']
+			)
+			users << author if author
+    		users << assigned_to if assigned_to
+			users
+				.uniq
+				.sort
 		end
 	end
 end
